1cccb3a795a3d2a87425fb77e9b2a3d9c8a76e29
Vinicius Fernandes <viniciusffj@gmail.com>
2019-03-19 19:45:49 +0100

    Add Company controller to have a create endpoint

---
+++ src/main/java/amazing/co/controllers/CompanyController.java
@@ -0,0 +1,23 @@
+package amazing.co.controllers;
+
+import amazing.co.models.Company;
+import amazing.co.services.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CompanyController {
+    private CompanyService companyService;
+
+    @Autowired
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @PostMapping("/companies")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody Company company) {
+        this.companyService.insert(company);
+    }
+}
---
+++ src/test/java/amazing/co/controllers/CompanyControllerComponentTest.java
@@ -0,0 +1,35 @@
+package amazing.co.controllers;
+
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class CompanyControllerComponentTest {
+
+    @Before
+    public void setUp() throws Exception {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 8080;
+    }
+
+    @Test
+    public void shouldCreateCompany() {
+        given()
+                .contentType("application/json")
+                .body("{ \"name\": \"duda's\" }")
+        .when()
+                .post("/companies")
+        .then()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+}
