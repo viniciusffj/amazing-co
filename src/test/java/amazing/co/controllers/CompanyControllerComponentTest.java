d6997211094f4506c9dda99219bbf2c431c20167
Vinicius Fernandes <viniciusffj@gmail.com>
2019-03-20 22:05:41 +0100

    Create ComponentTest helper to reuse setup code

--- src/test/java/amazing/co/controllers/CompanyControllerComponentTest.java
+++ src/test/java/amazing/co/controllers/CompanyControllerComponentTest.java
@@ -1,30 +1,12 @@
 package amazing.co.controllers;
 
-import io.restassured.RestAssured;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasKey;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CompanyControllerComponentTest {
-
-    @LocalServerPort
-    int serverPort;
-
-
-    @Before
-    public void setUp() throws Exception {
-        RestAssured.baseURI = "http://localhost";
-        RestAssured.port = serverPort;
-    }
+public class CompanyControllerComponentTest extends ComponentTest {
 
     @Test
     public void shouldCreateAndDeleteCompany() {
