ec6660bb8ef1c7ee262ba5bb680b7c2fb68bf98f
Vinicius Fernandes <viniciusffj@gmail.com>
2019-03-23 18:44:26 +0100

    Delete redundant test in node repository
    Those tests made sense in the beginning of development,
    as it checked if our mapping was correct. Because other
    tests already create root and non root nodes, they became
    unnecessary

--- src/test/java/amazing/co/repositories/NodeRepositoryIntegrationTest.java
+++ src/test/java/amazing/co/repositories/NodeRepositoryIntegrationTest.java
@@ -2,7 +2,6 @@ package amazing.co.repositories;
 
 import amazing.co.models.Company;
 import amazing.co.models.Node;
-import org.assertj.core.util.Lists;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
 import java.util.Optional;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -38,32 +36,6 @@ public class NodeRepositoryIntegrationTest {
     public void tearDown() throws Exception {
         nodeRepository.deleteAll();
         companyRepository.deleteAll();
-    }
-
-    @Test
-    public void shouldCreateRootNode() {
-        Node node = Node.rootNode("Root", awesomeCompany);
-        nodeRepository.save(node);
-
-        ArrayList<Node> nodes = Lists.newArrayList(nodeRepository.findAll());
-        assertThat(nodes).hasSize(1);
-        assertThat(nodes.get(0)).isEqualTo(node);
-    }
-
-    @Test
-    public void shouldCreateNonRootNode() {
-        Node root = Node.rootNode("Root", awesomeCompany);
-        nodeRepository.save(root);
-
-        Node node = Node.nonRootNode("Non Root", root, root);
-        Node expectedNode = nodeRepository.save(node);
-
-        ArrayList<Node> nodes = Lists.newArrayList(nodeRepository.findAll());
-        assertThat(nodes).hasSize(2);
-
-        assertThat(expectedNode.getCompany()).isEqualTo(awesomeCompany);
-        assertThat(expectedNode.getRoot()).isEqualTo(root);
-        assertThat(expectedNode.getParent()).isEqualTo(root);
     }
 
     @Test
